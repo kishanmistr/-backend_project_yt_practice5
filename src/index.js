f28762b9c191479b21e23733de9209b558d343e8
kiritDholakiya <dolakiyakirit@gmail.com>
2024-05-29 12:42:23 +0530

    commit this

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import app from './app.js'
+
+dotenv.config({
+
+    path : "./env"
+
+})
+
+connectDB()
+.then( () => {
+
+    app.get('/' , (req,res) => {
+
+        res.send('<h1>hi kishan mistry</h1>')
+
+    } )
+
+    app.listen(process.env.PORT || 3000  , () => {
+
+        console.log(`  server at http://localhost:${process.env.PORT} `);
+
+    } )
+
+
+} )
+.catch((err) => {
+
+    console.log('Mongodb Connection Failed' ,err);
+
+
+} )
---
+++ src/utils/ApiError.js
@@ -0,0 +1,40 @@
+class ApiError extends  Error {
+    
+
+    constructor(
+
+        statusCode ,
+        message = "Something Went Wrong",
+        errors = [],
+        stack = ""
+
+    )
+
+    {
+
+
+        this.statusCode = statusCode
+        this.message = message
+        this.data = null
+        this.success = false
+        this.errors = errors
+
+
+        if(stack) {
+
+            this.stack = stack
+
+        }
+
+        else {
+
+            Error.captureStackTrace(this,this.constructor)
+
+        }
+
+    }
+
+
+}
+
+export default  ApiError
